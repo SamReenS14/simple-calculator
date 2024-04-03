cd46c4fc587ccadf93b87e230d545a5311a7043e
SamReenS14 <samreenaman87@gmail.com>
2024-04-03 13:06:34 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter frist number", type: "number", name: "fristNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of operator to operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "multiplication", "Division"],
+    },
+]);
+//conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.fristNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.fristNumber - answer.secondNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.fristNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.fristNumber / answer.secondNumber);
+}
+else {
+    console.log("please select valid number");
+}
